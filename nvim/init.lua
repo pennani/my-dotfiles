ba2bb887bc08603b7d50213494ce6bd1ea666615
pennani <marcuspennanen02@gmail.com>
2025-11-25 12:15:41 +0100

    Fixed the kitty keybind for saving dotfiles.

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Idk if i should have this enabled. If clipboard ever starts tweaking you know why
 -- vim.opt.cliboard = "unnamedplus"
